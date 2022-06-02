6de4836421da1b409b9a7bf0d9eac40c80e15dea
Maria Isabel Mendonça da Silva <mariaisabelmds1@gmail.com>
2022-06-02 08:49:56 -0300

    modificando estilização

--- modulo1/projeto-whatslab/projeto-whatslab/src/Components/SendMessage.js
+++ modulo1/projeto-whatslab/projeto-whatslab/src/Components/SendMessage.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
 const MessageButton = styled.div `
     display: flex;
     justify-content: center;
+    margin: 2px;
 `
 
 const Data = styled.input `
     width: 100px;
-    padding: 0px 2px;
+    padding: 0px 4px;
+    border-radius: 5px;
 `
 const Message = styled.input `
-    width: 30%;
+    width: 60%;
     margin: 0px 1px;
+    padding: 0px 4px;
+    border-radius: 5px;
+`
+
+const MessageContainer = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-itens: center;
+    border: 2px solid black;
+    border-radius: 20px;
+    height: 650px;
+    width: 450px;
+    justify-content: flex-end;
+    margin: 3px auto;
 `
 
 function SendMessage() {
     return(
-        <div>
+        <MessageContainer>
             <div>
             </div>
             <MessageButton>
@@ -25,7 +42,7 @@ function SendMessage() {
                 <Message type="text" placeholder="Digite aqui sua mensagem:"/>
                 <button>Enviar</button>
             </MessageButton>
-        </div>
+        </MessageContainer>
     )
 }
 
