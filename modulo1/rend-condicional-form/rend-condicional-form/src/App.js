38c325a5b8d64dd6e9ca1a3eb0e9bce91f88de20
Maria Isabel Mendonça da Silva <mariaisabelmds1@gmail.com>
2022-06-07 23:43:47 -0300

    Criando labenuform

--- modulo1/rend-condicional-form/rend-condicional-form/src/App.js
+++ modulo1/rend-condicional-form/rend-condicional-form/src/App.js
@@ -1,10 +1,43 @@
 // import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import styled from "styled-components";
+import { Etapa1 } from './components/Etapa1';
+import { Etapa2 } from './components/Etapa2';
+import { Etapa3 } from './components/Etapa3';
+import { EtapaFinal } from './components/Final';
+
+const Formulario = styled.div `
+margin: 20px auto;
+text-align: center;;
+`
 
 function App() {
+  const [etapa, setEtapa] = useState(1)
+
+  const onChangeSetEtapa = () => {
+    setEtapa (etapa + 1)
+  }
+
+  const renderizaForm = () => {
+    switch (etapa) {
+      case 1:
+        return <Etapa1/>;
+      case 2:
+        return <Etapa2/>;
+      case 3:
+        return <Etapa3/>;
+      case 4:
+        return <EtapaFinal/>
+        default: break;
+    } console.log(renderizaForm)
+  }
+
   return (
-    <div className="App">
-    </div>
+    <Formulario>
+      {renderizaForm()}
+      <button onClick={onChangeSetEtapa}>Próxima etapa</button>
+    </Formulario>
   );
 }
 
