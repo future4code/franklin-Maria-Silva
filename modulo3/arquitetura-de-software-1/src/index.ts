5fb026e7208bc8c103e74f771d1235c2fdba3a5a
Maria Isabel Mendonca da Silva <mariaisabelmds1@gmail.com>
2022-09-14 11:46:53 -0300

    continuacao exercicio arquitetura

--- modulo3/arquitetura-de-software-1/src/index.ts
+++ modulo3/arquitetura-de-software-1/src/index.ts
@@ -1,2 +1,23 @@
 // adicionar rotas
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { AddressInfo } from "net";
+import UserController from './controller/UserController'
 
+const app = express();
+app.use(express.json());
+app.use(cors())
+dotenv.config();
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});;
+
+const userController = new UserController();
+app.post("/users/signup", userController.signup);
