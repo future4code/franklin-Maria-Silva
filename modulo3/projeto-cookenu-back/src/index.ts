28939131dba99ff6264229987106df55fc33033f
Maria Isabel Mendonca da Silva <mariaisabelmds1@gmail.com>
2022-09-23 11:31:43 -0300

    configurando projeto

--- modulo3/projeto-cookenu-back/src/index.ts
+++ modulo3/projeto-cookenu-back/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from "dotenv"
-import { UserController } from './controller/UserController'
+import { userRouter } from './router/userRouter'
 
 dotenv.config()
 
@@ -13,10 +13,4 @@ app.listen(process.env.PORT || 3003, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
 })
 
-const userController = new UserController()
-
-app.post("/users/signup", userController.signup)
-app.post("/users/login", userController.login)
-app.get("/users/", userController.getUsers)
-app.delete("/users/:id", userController.deleteUser)
-app.put("/users/:id", userController.editUser)
+app.post("/users", userRouter)
